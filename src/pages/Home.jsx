992376ba0c87ce2441d6d8c66cda9f1e5181b90f
Grant Finn <fosterffinn@gmail.com>
2025-07-29 19:20:26 -0400

    Initial commit with basic React setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div style={{ padding: '2rem', textAlign: 'center' }}>
+            <p>
+                Hi, I'm [Your Name], a passionate developer. Explore my projects and experience!
+            </p>
+        </div>
+    );
+}
+
+export default Home;
