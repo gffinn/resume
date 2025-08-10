cd7975b62716e25a56a63a48dc3c3963c3f4e28d
Grant Finn <fosterffinn@gmail.com>
2025-08-10 15:59:01 -0400

    Header Commits

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Header.module.css'; 
+
+const Header = () => (
+    <header style={{ padding: '1rem', background: '#282c34', color: '#fff' }}>
+        <h1 className={styles.title}>Grant   F.     Finn</h1>
+    </header>
+);
+
+export default Header;
