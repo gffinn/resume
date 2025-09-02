d02fbca3729b2d0fbd37c41c83fcbcac1c770e1b
Grant Finn <finng@umich.edu>
2025-09-02 15:25:39 -0400

    Header to Resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,11 @@
+import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 
 export default function Resume() {
     return (
         <>
         <NavBar />
-        <h1>Resume</h1>
-        <p>This is the Resume page.</p>
+        <Header />
         </>
 
     )
