b77c2e19cc237d875b0c69fa06a1a94b4c7ef588
grant <grant@ADMINIS-143U0SS>
2025-09-09 16:54:22 -0400

    Modify the Header and Home components to adjust font sizes and layout. removed the subtitle from the Header component. added shimmer effect to the title text in Header. wrapped the timeline in a div with a specific class for styling. enlarged header to expand view area.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,8 @@ export default function Header(){
 return (
     <header className={styles.background}>
         <h1 className={styles.title}>Grant F. Finn</h1>
-        <p className={styles.subtitle}>Software Engineer | Web Dev | AI Student</p>
+  {/*      <p className={styles.subtitle}>Software Engineer | Web Dev | AI Student</p>
+  */}
     </header>
 );
 }
