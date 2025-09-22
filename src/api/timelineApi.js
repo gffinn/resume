e17c97619f9acb514d75b8bd3a9054986dac859b
Grant Finn <finng@umich.edu>
2025-09-22 18:18:16 -0400

    Timeline in the resume page

---
+++ src/api/timelineApi.js
@@ -0,0 +1,8 @@
+import timelineData from '../components/data/Timeline.data'
+
+// Simulated API: returns a Promise that resolves to the timeline data after a short delay.
+export function fetchTimelineData({ delay = 300 } = {}) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(JSON.parse(JSON.stringify(timelineData))), delay)
+  })
+}
