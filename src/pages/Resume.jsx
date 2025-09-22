f5b27fbb624c23611a407cd855747521e544d204
grant <grant@ADMINIS-143U0SS>
2025-09-22 18:18:16 -0400

    Timeline in the resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
+import Timeline from "../components/Timeline";
 
 export default function Resume() {
     return (
         <>
             <NavBar />
             <Header />
+            <Timeline />
         </>
     )
 }
