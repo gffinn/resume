ca754d32e505bee315b73640681e294225472357
Grant Finn <finng@umich.edu>
2025-11-05 17:04:11 -0500

    style changes. ada compliance updates.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,8 +7,8 @@ export default function NavBar() {
             <div className={styles.links}>
                 <Link to="/" className={styles.linkItem}>Home</Link>
                 <Link to="/Resume" className={styles.linkItem}>Resume</Link>
-                <Link to="/CodingChallenges" className={styles.linkItem}>Engineering Challenges</Link>
-                <Link to="/Announcements" className={styles.linkItem}>Announcements</Link>
+                {/* <Link to="/CodingChallenges" className={styles.linkItem}>Engineering Challenges</Link> */}
+                {/* <Link to="/Announcements" className={styles.linkItem}>Announcements</Link> */}
                 <div className={styles.initials} aria-hidden="true">GF</div>
             </div>
         </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,39 +15,42 @@ const items = [
     text: "Resume",
     url: "/Resume",
     newTab: false,
+    desc: "Checkout my resume. I really did all those things, and have stories to prove it."
   },
   {
     icon: FaLinkedin,
     text: "LinkedIn",
     url: "https://www.linkedin.com/in/grant-f-finn/",
     newTab: true,
+    desc: "Follow me and see what doing 10 pushups a day taught me about B2B sales on LinkedIn."
   },
   {
     icon: FaGithub,
     text: "GitHub",
     url: "https://github.com/gffinn",
     newTab: true,
+    desc: "Be my friend on GitHub too!"
   },
-  {
-    icon: FaGears,
-    text: "Coding Challenges",
-    url: "/#CodingChallenges",
-    newTab: false,
-  },
-  {
-    icon: AiFillNotification,
-    text: "Announcements",
-    url: "/#Announcements",
-    newTab: false,
-  },
+  // {
+  //   icon: FaGears,
+  //   text: "Coding Challenges",
+  //   url: "/#CodingChallenges",
+  //   newTab: false,
+  // },
+  // {
+  //   icon: AiFillNotification,
+  //   text: "Announcements",
+  //   url: "/#Announcements",
+  //   newTab: false,
+  // },
 ];
 
 export default function Home() {
   return (
     <div>
       <NavBar />
-      <Header className={Styles.header}/>
-      <SiteDescription className={Styles.siteDescription}/>
+      <Header className={Styles.header} />
+      <SiteDescription className={Styles.siteDescription} />
       <div className={Styles.timeline}>
         {items.map((item, index) => (
           <motion.div
@@ -70,11 +73,12 @@ export default function Home() {
                 newTab={item.newTab}
               />
             </div>
-
-            {/* Text */}
-            <div className={Styles.textBox}>
-              {item.text}
-            </div>
+            {/* Description */}
+            {item.desc && (
+              <div className={Styles.descriptionBox}>
+                <p>{item.desc}</p>
+              </div>
+            )}
           </motion.div>
         ))}
       </div>
