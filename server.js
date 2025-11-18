1e695c6348e55635446340bdf4a750ad0d1d7cda
Grant Finn <finng@umich.edu>
2025-11-18 13:06:43 -0500

    express addition

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Serve React build folder
+app.use(express.static(path.join(__dirname, "build")));
+
+// Handle React routing, serve index.html for all other requests
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
