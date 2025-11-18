eb0f94822a8b496cb4121ff6c44df62e0096ad38
Grant Finn <finng@umich.edu>
2025-11-18 13:35:22 -0500

    Fix Express 5 catch-all route for Azure deployment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, "build")));
 
 // Handle React routing, serve index.html for all other requests
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
